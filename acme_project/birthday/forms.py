5dab7ca3d07e5efa6e2f1753e213d7efb951e686
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-04-10 21:35:21 +0300

    Creating records in the database using the form

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,13 +1,20 @@
+# birthday/forms.py
 from django import forms
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(label='Имя', max_length=20)
-    last_name = forms.CharField(
-        label='Фамилия',
-        required=False,
-        help_text='Необязательное поле'
-    )
-    birthday = forms.DateField(
-        label='Дата рождения',
-        widget=forms.DateInput(attrs={'type':'date'})
-    )
+# Импортируем класс модели Birthday.
+from .models import Birthday
+
+
+# Для использования формы с моделями меняем класс на forms.ModelForm.
+class BirthdayForm(forms.ModelForm):
+    # Удаляем все описания полей.
+
+    # Все настройки задаём в подклассе Meta.
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразит все поля.
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        } 
