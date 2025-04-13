0d52cfa0f637f7483c788ec011ca6479bbedb3d9
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-04-13 21:42:50 +0300

    add Static pages and the TemplateView class

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,8 +1,12 @@
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+# birthday/views.py
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
+from .utils import calculate_birthday_countdown
 
 class BirthdayListView(ListView):
     model = Birthday
@@ -12,16 +16,21 @@ class BirthdayListView(ListView):
 class BirthdayCreateView(CreateView):
     model = Birthday
     form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
-    success_url = reverse_lazy('birthday:list') 
 
 class BirthdayUpdateView(UpdateView):
     model = Birthday
     form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
-    success_url = reverse_lazy('birthday:list') 
 
 class BirthdayDeleteView(DeleteView):
     model = Birthday
-    success_url = reverse_lazy('birthday:list') 
+    success_url = reverse_lazy('birthday:list')
 
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
