2d08a3d8c3fc3b1a951edd7ef936396ea244aac8
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-04-13 19:28:03 +0300

    user can add profile image

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -8,10 +8,10 @@ from .validators import real_age
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
-        'Фамилия', max_length=20, help_text='Необязательное поле', blank=True
+        'Фамилия', max_length=20, help_text='Опциональное поле', blank=True
     )
-    # Валидатор указывается в описании поля.
     birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
 
     class Meta:
         constraints = (
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -5,23 +5,17 @@ from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
-# Добавим опциональный параметр pk.
 def birthday(request, pk=None):
-    # Если в запросе указан pk (если получен запрос на редактирование объекта):
     if pk is not None:
-        # Получаем объект модели или выбрасываем 404 ошибку.
         instance = get_object_or_404(Birthday, pk=pk)
-    # Если в запросе не указан pk
-    # (если получен запрос к странице создания записи):
     else:
-        # Связывать форму с объектом не нужно, установим значение None.
         instance = None
-    # Передаём в форму либо данные из запроса, либо None. 
-    # В случае редактирования прикрепляем объект модели.
-    form = BirthdayForm(request.POST or None, instance=instance)
-    # Остальной код без изменений.
+    form = BirthdayForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=instance
+    )
     context = {'form': form}
-    # Сохраняем данные, полученные из формы, и отправляем ответ:
     if form.is_valid():
         form.save()
         birthday_countdown = calculate_birthday_countdown(
