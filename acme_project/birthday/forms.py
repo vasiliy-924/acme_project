a62502f6afb6e93e789187ed41824f516f4f2c3a
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-04-13 13:46:28 +0300

    added form validation

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,20 +1,35 @@
 # birthday/forms.py
 from django import forms
+from django.core.exceptions import ValidationError
+from .validators import real_age
+from .models import Birthday  # Импортируем модель
 
-# Импортируем класс модели Birthday.
-from .models import Birthday
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
-
-# Для использования формы с моделями меняем класс на forms.ModelForm.
 class BirthdayForm(forms.ModelForm):
-    # Удаляем все описания полей.
-
-    # Все настройки задаём в подклассе Meta.
+    first_name = forms.CharField(label='Имя', max_length=20)
+    last_name = forms.CharField(
+        label='Фамилия', required=False, help_text='Необязательное поле'
+    )
+    birthday = forms.DateField(
+        label='Дата рождения',
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        validators=(real_age,),
+    )
+    
     class Meta:
-        # Указываем модель, на основе которой должна строиться форма.
         model = Birthday
-        # Указываем, что надо отобразит все поля.
         fields = '__all__'
-        widgets = {
-            'birthday': forms.DateInput(format='%Y-%m-%d', attrs={'type': 'date'})
-        } 
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+        return first_name.split()[0]
+    
+    def clean(self):
+        super().clean()
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,10 +1,22 @@
 # birthday/models.py
 from django.db import models
 
+# Импортируется функция-валидатор.
+from .validators import real_age
+
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
-        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+        'Фамилия', max_length=20, help_text='Необязательное поле', blank=True
     )
-    birthday = models.DateField('Дата рождения')
+    # Валидатор указывается в описании поля.
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
