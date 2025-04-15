2fd0ce217c7e85037f680239eda52df9045524ad
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-04-15 22:33:07 +0300

    add email sending

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -91,3 +91,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = 'media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'  # Подключаем бэкенд filebased.EmailBackend:
+
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'  # Указываем директорию, в которую будут сохраняться файлы писем:
--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,6 +1,7 @@
 # birthday/forms.py
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
 from .validators import real_age
 from .models import Birthday  # Импортируем модель
 
@@ -30,6 +31,15 @@ class BirthdayForm(forms.ModelForm):
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
         if f'{first_name} {last_name}' in BEATLES:
+            # Отправляем письмо, если кто-то представляется 
+            # именем одного из участников Beatles.
+            send_mail(
+                subject='Another Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
-            )
+            ) 
