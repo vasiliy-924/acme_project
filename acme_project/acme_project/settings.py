dc615b38beaa40dc5732bd494f9c04d111b9305f
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-04-16 00:39:01 +0300

    the user registration page is set up

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'birthday.apps.BirthdayConfig',
     'pages.apps.PagesConfig',
+    'users.apps.UsersConfig',
     'django_bootstrap5',
 ]
 
--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,11 +1,22 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import include, path, reverse_lazy
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/', 
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
